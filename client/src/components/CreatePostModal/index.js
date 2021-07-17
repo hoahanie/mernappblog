2fb06b9b31abe8abb73fc1e7f040cb1fa3b17d0b
hoahanie <hoale160799@gmail.com>
2021-07-17 19:36:52 +0700

    init

---
+++ client/src/components/CreatePostModal/index.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { Button, Modal, TextareaAutosize, TextField } from "@material-ui/core";
+import { useSelector, useDispatch } from "react-redux";
+import { modalState$ } from "../../redux/selectors";
+import useStyles from "./styles";
+import FileBase64 from "react-file-base64";
+import { hideModal, createPost } from "../../redux/actions";
+
+export default function CreatePostModal() {
+  const [data, setData] = React.useState({
+    title: "",
+    content: "",
+    attachment: "",
+  });
+  const { isShow } = useSelector(modalState$);
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const onClose = React.useCallback(() => {
+    dispatch(hideModal());
+    setData({
+      title: '',
+      content: '',
+      attachment: '',
+    });
+  }, [dispatch]);
+
+  const onSubmit = React.useCallback(() => {
+    console.log({ data });
+    dispatch(createPost.createPostRequest(data));
+    onClose();
+  }, [data, dispatch,onClose]);
+
+  const body = (
+    <div className={classes.paper} id="simple-modal-title">
+      <h2>Create New Post</h2>
+      <form noValidate autoComplete="off" className={classes.form}>
+        <TextField
+          className={classes.title}
+          required
+          label="Title"
+          value={data.title}
+          onChange={(e) => setData({ ...data, title: e.target.value })}
+        />
+        <TextareaAutosize
+          className={classes.textarea}
+          rowsMin={10}
+          rowsMax={15}
+          placeholder="Content..."
+          value={data.content}
+          onChange={(e) => setData({ ...data, content: e.target.value })}
+        />
+        <FileBase64
+          accept='image/*'
+          multiple={false}
+          type='file'
+          value={data.attachment}
+          onDone={({ base64 }) => setData({ ...data, attachment: base64 })}
+        />
+        <div className={classes.footer}>
+          <Button
+            variant='contained'
+            color='primary'
+            component='span'
+            fullWidth
+            onClick={onSubmit}
+          >
+            {" "}
+            Create
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+
+  console.log(isShow);
+  return (
+    <div>
+      <Modal open={isShow} onClose={onClose}>
+        {body}
+      </Modal>
+    </div>
+  );
+}
